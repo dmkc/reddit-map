d896cd171a84f3c23ff0f41148574055fa8a7605
Dmitry Kichenko <dmitrykichenko@gmail.com>
2012-05-28 19:35:05 -0400

    Add scrolling of map

--- Voyager I  When Engineers Were Engineers who could code an Operating System for a Freakin' Spaceship in 68 Freakin' Kilobytes! 35 years later and still accepting commands.   programming_files/lens.js
+++ Voyager I  When Engineers Were Engineers who could code an Operating System for a Freakin' Spaceship in 68 Freakin' Kilobytes! 35 years later and still accepting commands.   programming_files/lens.js
@@ -1,6 +1,10 @@
 $(function(){
-    // Lens!
+    // Lens panel
     var lens = $(document.createElement('div')).prop('id', 'reddit-lens'),
+        content = $(document.createElement('div')).addClass('lens-content'),
+        height_ratio = 0,
+        
+   // Comment DOM nodes
         top_level_comments = $('.commentarea').children('.sitetable').children('.comment'),
         parse_comments = function( parent, nodes ) {
             $.each( nodes, function(){
@@ -11,6 +15,9 @@ $(function(){
                     children = $( document.createElement('div')).addClass('lens-comment-children');
 
                 comment.append( node ).append( children );
+                // keep track of the original DOM node for the comment
+                comment.data( 'orig', this );
+
                 parent.append( comment );
 
                 // recurse on the comment's children
@@ -20,7 +27,18 @@ $(function(){
             });
         };
 
+    lens.append( content );
     lens.insertBefore( $(document.body).children().eq(0));
-    parse_comments( lens, top_level_comments );
+    parse_comments( content, top_level_comments );
 
+    height_ratio = content.height() / $(document).height();
+
+    // Scroll lens proportionally with viewport
+    $(document).scroll( function() {
+        var offset = ( window.pageYOffset + $(window).height()/2 ) * height_ratio;
+
+        if ( offset + $(window).height() <= content.height() ) {
+            content.css( 'margin-top', -offset + 15 );
+        }
+    });
 });
