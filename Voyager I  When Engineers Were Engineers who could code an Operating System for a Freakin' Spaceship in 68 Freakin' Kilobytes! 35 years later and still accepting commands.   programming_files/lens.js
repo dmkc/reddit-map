2dec7de504c79e69ebc3c5408bb44d084550c53d
Dmitry Kichenko <dmitrykichenko@gmail.com>
2012-05-28 18:24:55 -0400

    Fix recursive implementation. Basic styling.

--- Voyager I  When Engineers Were Engineers who could code an Operating System for a Freakin' Spaceship in 68 Freakin' Kilobytes! 35 years later and still accepting commands.   programming_files/lens.js
+++ Voyager I  When Engineers Were Engineers who could code an Operating System for a Freakin' Spaceship in 68 Freakin' Kilobytes! 35 years later and still accepting commands.   programming_files/lens.js
@@ -4,17 +4,18 @@ $(function(){
         top_level_comments = $('.commentarea').children('.sitetable').children('.comment'),
         parse_comments = function( parent, nodes ) {
             $.each( nodes, function(){
-                var that = $( this ),
-                    comment = $(document.createElement('div')).addClass('lens-comment'),
-                    node = $( document.createElement('div')).addClass('lens-comment-node'), 
+                var $n = $(this),
+                    comment_children = $n.children('.child').children('.sitetable').children('.comment'),
+                    comment  = $(document.createElement('div')).addClass('lens-comment'),
+                    node     = $( document.createElement('div')).addClass('lens-comment-node'), 
                     children = $( document.createElement('div')).addClass('lens-comment-children');
 
                 comment.append( node ).append( children );
                 parent.append( comment );
 
                 // recurse on the comment's children
-                if( that.children().length !== 0 ) {
-                    parse_comments( children, that.children() );
+                if( comment_children.length != 0 ) {
+                    parse_comments( children, comment_children );
                 }
             });
         };
