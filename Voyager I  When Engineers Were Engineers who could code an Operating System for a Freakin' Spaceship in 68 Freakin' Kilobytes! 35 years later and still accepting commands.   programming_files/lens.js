57bf2b2ed2ea4cd4b2ad6258e092367d050c7e9d
Dmitry Kichenko <dmitrykichenko@gmail.com>
2012-05-30 20:12:14 -0400

    Add scrolling of viewport when dragging lens

--- Voyager I  When Engineers Were Engineers who could code an Operating System for a Freakin' Spaceship in 68 Freakin' Kilobytes! 35 years later and still accepting commands.   programming_files/lens.js
+++ Voyager I  When Engineers Were Engineers who could code an Operating System for a Freakin' Spaceship in 68 Freakin' Kilobytes! 35 years later and still accepting commands.   programming_files/lens.js
@@ -1,13 +1,34 @@
 $(function(){
-    // map panel
-    var map = $(document.createElement('div')).prop('id', 'reddit-map'),
-        content = $(document.createElement('div')).addClass('map-content'),
+    // Create map panel
+    var map          = $(document.createElement('div')).prop('id', 'reddit-map'),
+        content      = $(document.createElement('div')).addClass('reddit-map-content'),
+        lens         = $( document.createElement('div')).addClass('reddit-map-lens'),
         height_ratio = 0,
         
    // Comment DOM nodes
-        top_level_comments = $('.commentarea').children('.sitetable').children('.comment'),
-        parse_comments = function( parent, nodes ) {
-            $.each( nodes, function(){
+        reddit_content = $('.commentarea').children('.sitetable')
+        reddit_comments = reddit_content.children('.comment'),
+        init_map = function() { 
+            // Lens dragging behaviour
+            lens.draggable({ axis: 'y' });
+            resize_lens();
+
+            $(document).bind( 'scroll', scroll_map );
+
+            lens.bind( 'drag', function(e,ui) {
+                // drop the scroll handler or it'll mess up our scrolling
+                //$(document).unbind( 'scroll', scroll_map );
+                //lens_move_handler( e, ui )
+            });
+
+            lens.bind( 'dragstop', function(e,ui) {
+                // drop the scroll handler or it'll mess up our scrolling
+                //$(document).bind( 'scroll', scroll_map );
+                lens_move_handler( e, ui )
+            });
+        },
+        comment_tree = function( parent, nodes ) {
+            $.each( nodes, function(){//{{{
                 var $n = $(this),
                     comment_children = $n.children('.child').children('.sitetable').children('.comment'),
                     comment  = $(document.createElement('div')).addClass('reddit-map-comment'),
@@ -22,23 +43,41 @@ $(function(){
 
                 // recurse on the comment's children
                 if( comment_children.length != 0 ) {
-                    parse_comments( children, comment_children );
-                }
+                    comment_tree( children, comment_children );
+                }//}}}
             });
+        },
+
+        // Scroll map proportionally to the document
+        scroll_map = function() {
+            var offset = ( window.pageYOffset  ) * height_ratio;
+
+            if ( offset + $(window).height() <= content.height() ) {
+                content.css( 'margin-top', -offset + 15 );
+            }
+            resize_lens();
+        }
+        
+        // Scroll document if user moves lens
+        lens_move_handler = function( e, ui ) {
+            $('html,body').animate( { 
+                scrollTop: ( window.pageYOffset * height_ratio + ui.position.top ) / height_ratio },
+            300);
+            lens.animate( { top: 0 }, 300 );
+        },
+
+        // Resize lens based on window height
+        resize_lens = function(e){
+            lens.height( $(window).height() * height_ratio );
         };
 
-    map.append( content );
+    map.append( lens, content );
     map.insertBefore( $(document.body).children().eq(0));
-    parse_comments( content, top_level_comments );
+    comment_tree( content, reddit_comments );
 
     height_ratio = content.height() / $(document).height();
 
-    // Scroll map proportionally with viewport
-    $(document).scroll( function() {
-        var offset = ( window.pageYOffset + $(window).height()/2 ) * height_ratio;
+    // Vroom!
+    init_map();
 
-        if ( offset + $(window).height() <= content.height() ) {
-            content.css( 'margin-top', -offset + 15 );
-        }
-    });
 });
